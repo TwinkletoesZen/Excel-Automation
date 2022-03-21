0e60de72733d8f12577a42c8465181f3fe914446
TwinkletoesZen <contact@twinkletoeszen.com>
2022-03-20 21:40:30 -0700

    Update formate

--- recorder.py
+++ recorder.py
@@ -11,15 +11,16 @@ ws_soccer = wb["Soccer"]
 ws_ping_pong = wb["Ping-Pong"]
 ws_badminton = wb["Badminton"]
 ws_pool = wb["Pool"]
-ws_standing = wb["Standing"]
-ws_All_House = wb["All House"]
+# ws_standing = wb["Standing"]
+# ws_All_House_NumWin = wb["All House"]
+# ws_All_House_NumPoint = wb["Game Points"]
 
-Unity = "Unity"
-Courage = "Courage"
-Discovery = "Discovery"
-Legacy = "Legacy"
-Spirit = "Spirit"
-Harmony = "Harmony"
+Unity = "UNITY"
+Courage = "COURAGE"
+Discovery = "DISCOVERY"
+Legacy = "LEGACY"
+Spirit = "SPIRIT"
+Harmony = "HARMONY"
 
 What_Game = input("What Game is it?: ").upper()
 
@@ -27,11 +28,24 @@ What_Game = input("What Game is it?: ").upper()
 
 def Record_Basketball_Game():
   Between_Who = input("What House is it Between?: ")
-  Winner = input("Winner: ")
+  Winner = input("Winner: ").upper()
   Loser = input("Loser: ")
   Points_Winner = input("Points for Winner: ")
   Points_Loser = input("Points for Loser: ")
   ask_Date = input("Is the game happening today?: ").upper()
+
+  # if Winner == Unity:
+  #   with open("House_Points/UnityP.txt", a):
+
+  #   with open("House_Wins/Unity.txt", a):
+
+  # if Loser == Unity:
+  #   with open("House_Points/UnityP.txt", a):
+
+  #   with open("House_Wins/Unity.txt", a):
+
+
+  
 
   
   if ask_Date == "Y":
