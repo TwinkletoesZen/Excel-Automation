6a9236b383f85eb98068f852bf6dbd383967d7ec
TwinkletoesZen <contact@twinkletoeszen.com>
2022-03-20 21:02:08 -0700

    Basic Recording Function working

---
+++ recorder.py
@@ -0,0 +1,147 @@
+from openpyxl import Workbook, load_workbook
+from datetime import date
+
+
+wb = Workbook()
+wb = load_workbook("Olympic.xlsx")
+
+ws_basketball = wb["Basketball"]
+ws_volleyball = wb["Volleyball"]
+ws_soccer = wb["Soccer"]
+ws_ping_pong = wb["Ping-Pong"]
+ws_badminton = wb["Badminton"]
+ws_pool = wb["Pool"]
+ws_standing = wb["Standing"]
+ws_All_House = wb["All House"]
+
+Unity = "Unity"
+Courage = "Courage"
+Discovery = "Discovery"
+Legacy = "Legacy"
+Spirit = "Spirit"
+Harmony = "Harmony"
+
+What_Game = input("What Game is it?: ").upper()
+
+
+
+def Record_Basketball_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_basketball.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])    
+
+def Record_Volleyball_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_volleyball.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])   
+ 
+def Record_Soccer_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_soccer.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])    
+
+def Record_PingPong_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_ping_pong.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])    
+
+def Record_Badminton_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_badminton.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])    
+
+def Record_Pool_Game():
+  Between_Who = input("What House is it Between?: ")
+  Winner = input("Winner: ")
+  Loser = input("Loser: ")
+  Points_Winner = input("Points for Winner: ")
+  Points_Loser = input("Points for Loser: ")
+  ask_Date = input("Is the game happening today?: ").upper()
+
+  
+  if ask_Date == "Y":
+    Date = date.today()
+  elif ask_Date == "N":
+    Date = input("Enter the Date (yyyy/mm/dd)")
+
+    
+  ws_pool.append([Between_Who, Winner, Loser, Points_Winner, Points_Loser, Date])    
+
+
+
+if What_Game == "BASKETBALL":
+  Record_Basketball_Game()
+elif What_Game == "VOLLEYBALL":
+  Record_Volleyball_Game()
+elif What_Game == "SOCCER":
+  Record_Soccer_Game()
+elif What_Game == "PINGPONG":
+  Record_PingPong_Game()
+elif What_Game == "BADMINTON":
+  Record_Badminton_Game()
+elif What_Game == "POOL":
+  Record_Pool_Game()
+
+
+
+wb.save("Olympic.xlsx")
